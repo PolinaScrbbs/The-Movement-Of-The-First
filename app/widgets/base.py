77cfc99aeb2672a33d3980fd272e77abc3f87acc
PolinaScrbbs <147543684+PolinaScrbbs@users.noreply.github.com>
2024-12-03 12:18:33 +0300

    Update BasePage

--- app/widgets/base.py
+++ app/widgets/base.py
@@ -18,20 +18,8 @@ class BaseApp(tk.Tk):
         # Навбар
         self.create_navbar(self.user)
 
-        # Футер
-        self.footer = tk.Frame(self, bg="lightgray", height=30)
-        self.footer.pack(side=tk.BOTTOM, fill=tk.X)
-
-        self.footer_label = tk.Label(
-            self.footer,
-            text="Powered by PolinaScrbbs",
-            font=("Arial", 10),
-            bg="lightgray",
-        )
-        self.footer_label.pack(side=tk.RIGHT, padx=10)
-
         # Основная область контента
-        self.content_frame = tk.Frame(self, bg="white")
+        self.content_frame = tk.Frame(self, background="#1a76b9")
         self.content_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
         self.show_content("Page 1")
@@ -44,7 +32,7 @@ class BaseApp(tk.Tk):
         self.user = user
 
         """Создание навбара в зависимости от наличия токена"""
-        self.navbar = tk.Frame(self, bg="lightgray", height=50)
+        self.navbar = tk.Frame(self, height=50, background="#ffffff")
         self.navbar.pack(side=tk.TOP, fill=tk.X)
 
         self.navbar.columnconfigure(0, weight=1)
@@ -52,48 +40,48 @@ class BaseApp(tk.Tk):
         self.navbar.columnconfigure(2, weight=1)
 
         # Левый блок (иконка и название)
-        self.left_frame = tk.Frame(self.navbar, bg="lightgray")
-        self.left_frame.grid(row=0, column=0, sticky="w", padx=10)
+        self.left_frame = tk.Frame(self.navbar, background="#ffffff")
+        self.left_frame.grid(row=0, column=0, sticky="w")
 
+        # Подготовка иконки
         icon_image = Image.open("app/assets/logo.jpg").resize((225, 75))
         self.icon = ImageTk.PhotoImage(icon_image)
-        self.icon_label = tk.Label(self.left_frame, image=self.icon, bg="lightgray")
-        self.icon_label.pack(side=tk.LEFT)
 
-        self.app_name = tk.Label(
-            self.left_frame, text="TMOTF", font=("Arial", 16, "bold"), bg="lightgray"
-        )
-        self.app_name.pack(side=tk.LEFT, padx=5)
+        # Метка с иконкой
+        self.icon_label = tk.Label(self.left_frame, image=self.icon, bg="#ffffff")
+        self.icon_label.pack(side=tk.LEFT)
 
         if self.user:
             # Центральный блок (кнопки переключения для администратора)
-            self.center_frame = tk.Frame(self.navbar, bg="lightgray")
+            self.center_frame = tk.Frame(self.navbar, background="#ffffff")
             self.center_frame.grid(row=0, column=1)
 
             self.button1 = tk.Button(
                 self.center_frame,
                 text="Events",  # Кнопка для отображения событий
                 command=self.event_app.show_events,  # Используем метод из класса EventApp
+                bg="#ffffff",
             )
             self.button2 = tk.Button(
                 self.center_frame,
                 text="Rating",
                 command=self.rating_app.show_rating,
+                bg="#ffffff",
             )
 
             self.button1.pack(side=tk.LEFT, padx=5)
             self.button2.pack(side=tk.LEFT, padx=5)
 
             # Правый блок (профиль и ник)
-            self.right_frame = tk.Frame(self.navbar, bg="lightgray")
-            self.right_frame.grid(row=0, column=2, sticky="e", padx=10)
+            self.right_frame = tk.Frame(self.navbar, background="#ffffff")
+            self.right_frame.grid(row=0, column=2, sticky="e")
 
             # Имя пользователя
             self.user_name = tk.Label(
                 self.right_frame,
                 text=self.user.username,
                 font=("Arial", 12),
-                bg="lightgray",
+                bg="#ffffff",
             )
             self.user_name.pack(side=tk.LEFT, padx=5)
 
@@ -112,10 +100,10 @@ class BaseApp(tk.Tk):
             self.profile_label = tk.Label(
                 self.right_frame,
                 image=self.profile_icon,
-                bg="lightgray",
                 width=30,
                 height=30,
                 cursor="hand2",  # Сделаем аватар кликабельным
+                bg="#ffffff",
             )
             self.profile_label.pack(side=tk.LEFT)
 
@@ -124,13 +112,8 @@ class BaseApp(tk.Tk):
 
         else:
             # Правый блок без токена (кнопка "Войти")
-            self.right_frame = tk.Frame(self.navbar, bg="lightgray")
+            self.right_frame = tk.Frame(self.navbar, background="#ffffff")
             self.right_frame.grid(row=0, column=2, sticky="e", padx=10)
-
-            self.login_button = tk.Button(
-                self.right_frame, text="Войти", command=self.login_action
-            )
-            self.login_button.pack(side=tk.RIGHT, padx=10)
 
     def refresh_navbar_avatar(self, avatar_path):
         """Обновляет аватар в навбаре"""
@@ -150,18 +133,40 @@ class BaseApp(tk.Tk):
 
     def show_content(self, page_name=None):
         """Обновление основного контента"""
+        # Удаляем все виджеты из content_frame
         for widget in self.content_frame.winfo_children():
             widget.destroy()
 
         if not self.user:  # Если пользователь не авторизован
+            # Родительский контейнер для надписи и кнопки
+            container = tk.Frame(self.content_frame, bg="#1a76b9")
+            container.place(relx=0.5, rely=0.5, anchor="center")  # Центрируем контейнер
+
+            # Надпись
             label = tk.Label(
-                self.content_frame,
-                text="Добро пожаловать в TMOTF!\nПожалуйста, авторизуйтесь, чтобы продолжить.",
+                container,
+                text="Добро пожаловать в {Название}!\nПожалуйста, авторизуйтесь, чтобы продолжить.",
                 font=("Arial", 18),
                 justify="center",
                 wraplength=600,
+                bg="#1a76b9",
+                fg="#ffffff"
+                
             )
-            label.pack(pady=50)
+            label.pack(pady=10)  # Отступ между текстом и кнопкой
+
+            # Кнопка "Войти"
+            login_button = tk.Button(
+                container,
+                text="Войти",
+                font=("Arial", 14),
+                command=self.login_action,
+                width=15,
+                height=2,
+                background="#ffffff"
+            )
+            login_button.pack(pady=10)  # Отступ под кнопкой
+
         else:  # Для авторизованных пользователей
             if page_name is None:  # Если страница не указана, показываем первую
                 page_name = "Page 1"
