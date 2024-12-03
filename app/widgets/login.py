b5efb7d0766078d009b9dc92ce7349c3ba36e894
PolinaScrbbs <147543684+PolinaScrbbs@users.noreply.github.com>
2024-12-03 12:29:40 +0300

    Update LoginPage

--- app/widgets/login.py
+++ app/widgets/login.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 
 from ..datebase import get_session
 from ..queries import user_login
@@ -6,35 +7,74 @@ from ..queries import user_login
 
 class LoginForm(tk.Frame):
     def __init__(self, parent, app, on_switch_to_register):
-        super().__init__(parent)
+        super().__init__(parent, bg="#1a76b9")
         self.parent = parent
         self.app = app
         self.on_switch_to_register = on_switch_to_register
 
-        self.pack(pady=10)
+        # Установка центра контейнера
+        self.pack(expand=True, fill="both")  # Расширение фрейма
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        # Основной контейнер для выравнивания содержимого
+        container = tk.Frame(self, bg="#1a76b9")
+        container.grid(row=0, column=0)
+        container.grid_columnconfigure(0, weight=1)
 
         # Заголовок
-        tk.Label(self, text="Авторизация", font=("Arial", 18)).pack(pady=10)
+        tk.Label(
+            container,
+            text="Авторизация",
+            font=("Arial", 18),
+            bg="#1a76b9",
+            fg="#ffffff",
+        ).grid(row=0, column=0, pady=20)
 
         # Поле для имени пользователя
-        tk.Label(self, text="Имя пользователя").pack(anchor="w", padx=5)
-        self.username_entry = tk.Entry(self)
-        self.username_entry.pack(fill="x", padx=5, pady=5)
+        tk.Label(
+            container,
+            text="Имя пользователя",
+            font=("Arial", 12),
+            bg="#1a76b9",
+            fg="#ffffff",
+        ).grid(row=1, column=0, sticky="w", padx=20)
+        self.username_entry = tk.Entry(container, width=30, font=("Arial", 14))
+        self.username_entry.grid(row=2, column=0, pady=10, padx=20)
 
         # Поле для пароля
-        tk.Label(self, text="Пароль").pack(anchor="w", padx=5)
-        self.password_entry = tk.Entry(self, show="*")
-        self.password_entry.pack(fill="x", padx=5, pady=5)
+        tk.Label(
+            container,
+            text="Пароль",
+            font=("Arial", 12),
+            bg="#1a76b9",
+            fg="#ffffff",
+        ).grid(row=3, column=0, sticky="w", padx=20)
+        self.password_entry = tk.Entry(container, show="*", width=30, font=("Arial", 14))
+        self.password_entry.grid(row=4, column=0, pady=10, padx=20)
 
         # Кнопка авторизации
-        tk.Button(self, text="Войти", command=self.login).pack(pady=10)
-
-        # Кнопка для перехода к регистрации
         tk.Button(
-            self,
+            container,
+            text="Войти",
+            bg="#ffffff",
+            font=("Arial", 14),
+            command=self.login,
+            height=1,  # Высота кнопки
+            width=20,  # Ширина кнопки
+        ).grid(row=5, column=0, pady=20)
+
+        # Текст для перехода к регистрации
+        link = tk.Label(
+            container,
             text="У вас еще нет аккаунта? Зарегистрируйтесь",
-            command=self.on_switch_to_register,
-        ).pack(pady=5)
+            bg="#1a76b9",
+            fg="#ffffff",
+            font=("Arial", 12, "underline"),
+            cursor="hand2",
+        )
+        link.grid(row=6, column=0, pady=10)
+        link.bind("<Button-1>", lambda e: self.on_switch_to_register())
 
     def login(self):
         """Обрабатывает авторизацию пользователя."""
@@ -42,16 +82,16 @@ class LoginForm(tk.Frame):
         password = self.password_entry.get()
 
         if not username or not password:
-            tk.messagebox.showerror("Ошибка", "Введите имя пользователя и пароль!")
+            messagebox.showerror("Ошибка", "Введите имя пользователя и пароль!")
             return
 
         user, msg = user_login(get_session(), login=username, password=password)
 
         if user:
-            tk.messagebox.showinfo("Авторизация", msg)
+            messagebox.showinfo("Авторизация", msg)
             self.on_login_success(app=self.app, user=user)
         else:
-            tk.messagebox.showerror("Ошибка авторизациия", msg)
+            messagebox.showerror("Ошибка авторизации", msg)
 
     def on_login_success(self, app, user):
         """Обновляем интерфейс после успешной авторизации"""
